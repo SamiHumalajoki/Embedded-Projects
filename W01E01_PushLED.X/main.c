ebd21b5415b3d80af18e8058810f04c17d86e5fc
Sami Humalajoki <sami.humalajoki@gmail.com>
2020-10-30 13:03:17 +0200

    Add W01E01

---
+++ W01E01_PushLED.X/main.c
@@ -0,0 +1,27 @@
+/*
+ * DTEK0068 Embedded Microprocessor Systems
+ * Week 44 exercise
+ * Simple LED and switch application for the ATmega4809 PCB.
+ */
+
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    // Set PF5 (LED) as out
+    PORTF.DIR |= PIN5_bm;
+    PORTF.DIR &= ~PIN6_bm;
+    
+    // The superloop
+    while (1)
+    {
+        if (PORTF.IN & PIN6_bm) {
+            PORTF.OUT |= PIN5_bm;    
+        }
+        else {
+            PORTF.OUT &= ~PIN5_bm;
+        }
+    }
+}
